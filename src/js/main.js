4a4528f74a0dc0e774264c86c1bceb08b2e72085
jordancrettenand <122967586+jordancrettenand@users.noreply.github.com>
2024-11-13 15:13:07 +0100

    4eme partie marge et padding + javascript

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,34 @@ $(document).ready(function () {
   });
 });
 // -------------------------------------------------------------------------
+
+// -- Bouton scroll -----------------------------------------------------------------------
+// -------------------------------------------------------------------------
+
+// -- NEWSLETTER -----------------------------------------------------------------------
+function submitEmail() {
+  const email = document.getElementById("email-input").value;
+  if (email) {
+    alert("Vous êtes abonnez ! : " + email);
+    // Ici vous pouvez ajouter le code pour envoyer l'email à votre serveur
+  } else {
+    alert("Veuillez entrer une adresse email valide.");
+  }
+}
+// -------------------------------------------------------------------------
+
+// -- Barre de progression -----------------------------------------------------------------------
+window.onscroll = function () {
+  var d = document,
+    doc = d.documentElement;
+  var top = doc.scrollTop || window.pageYOffset || d.body.scrollTop,
+    het = d.getElementsByClassName("nav")[0].scrollHeight,
+    wid = doc.clientWidth || d.body.clientWidth;
+  var childs = d.querySelectorAll(".nav .child"),
+    s = childs[0].offsetTop;
+  var scroll = (top - s) * (wid / het);
+  scroll =
+    scroll >= het * (wid / het) ? het * (wid / het) : top - s < 10 ? 0 : scroll;
+  d.getElementsByClassName("progressBar")[0].style.width = scroll + "px";
+};
+// -------------------------------------------------------------------------
